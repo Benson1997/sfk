e683a30ea238d98b595bc29a4a3023d568114db6
Benson1997 <111626633+Benson1997@users.noreply.github.com>
2024-04-09 12:50:03 +0800

    sfk.js

--- sfk.js
+++ sfk.js
@@ -1,3 +1,27 @@
+settings.smoothScroll = true;
+const {
+    unmap,
+    iunmap,
+    vunmap,
+    aceVimMap,
+    mapkey,
+    imap,
+    imapkey,
+    getClickableElements,
+    vmapkey,
+    map,
+    cmap,
+    addSearchAlias,
+    removeSearchAlias,
+    tabOpenLink,
+    readText,
+    Clipboard,
+    Front,
+    Hints,
+    Visual,
+    RUNTIME
+} = api;
+
 addSearchAlias('p', 'duckduckgo', 'https://duckduckgo.com/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
     var res = JSON.parse(response.text);
     return res.map(function(r){
