f72f8e76f75a51e9c4ff7f49f69a20aa1886c4b3
Benson1997 <111626633+Benson1997@users.noreply.github.com>
2024-04-10 15:56:37 +0800

    Update sfk.js

--- sfk.js
+++ sfk.js
@@ -22,6 +22,9 @@ const {
     RUNTIME
 } = api;
 
+api.unmap('<c>');
+api.unmap('<shift-s>');
+
 addSearchAlias('p', 'duckduckgo', 'https://duckduckgo.com/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
     var res = JSON.parse(response.text);
     return res.map(function(r){
